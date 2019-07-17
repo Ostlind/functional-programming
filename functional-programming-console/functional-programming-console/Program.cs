34ccf93f456075f882ad565d833f8f80cf261c2d
Fabio Ostlind <fabio.ostlind@afconsult.com>
2019-07-17 13:10:53 +0200

    Add new folder.

---
+++ functional-programming-console/functional-programming-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace functional_programming_console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ functional-programming-console/functional-programming-console/scipts/load.csx
@@ -0,0 +1,3 @@
+﻿#r "C:\Users\A501205\.nuget\packages\layumba.functional\0.9.1\lib\netstandard1.6\LaYumba.Functional.dll"
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
